d2643dee18713f4c180626af08e8a36df0b13aa8
GABRIELA <gabsmomilli@gmail.com>
2024-01-18 00:41:49 -0300

    metade do exercicio de concessionaria

--- estudos/src/EstruturaSequencial/MetrosCentimetros.java
+++
@@ -1,9 +0,0 @@
-package EstruturaSequencial;
-
-public class MetrosCentimetros {
-    public static void main(String[] args) {
-        int metros = 56;
-        int centimetros = metros * 100;
-        System.out.println(metros + " metros sao " + centimetros + " centimetros");
-    }
-}
